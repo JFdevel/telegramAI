84d23bd0f06833d117aa1cbbc51456ca4b94c215
JFdevel <39263181+JFdevel@users.noreply.github.com>
2023-02-03 21:14:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def object_detection_on_an_image():
     segment_image = instance_segmentation()
     segment_image.load_model("mask_rcnn_coco.h5")
 
-    target_class = segment_image.select_target_classes(person=True)
+    # target_class = segment_image.select_target_classes(person=True)
 
     result = segment_image.segmentImage(
         # image_path="1city.jpg",
         # image_path="2cars_people.jpeg",
         image_path="3silicon_valley.jpg",
         # show_bboxes=True,
-        segment_target_classes=target_class,
+        # segment_target_classes=target_class,
         # extract_segmented_objects=True,
         # save_extracted_objects=True,
         output_image_name="output.jpg"
